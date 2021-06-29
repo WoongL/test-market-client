76626f0d8ac759f472cc4e6c7f58f5a57620e9c0
이웅 <dldnd2000kr@naver.com>
2021-06-29 20:18:32 +0900

    api 주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URI = "http://localhost:8080";
+export const API_URI =
+  process.env.NODE_ENV === "production"
+    ? "https://lw-test-market.herokuapp.com"
+    : "http://localhost:8080";
